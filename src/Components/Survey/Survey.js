a9758f54244400de61395cf0612858c8dc5bc62b
Joseph Rodrigo <113199438+itsjorigo@users.noreply.github.com>
2023-09-04 12:24:10 +0530

    09.04.23 - 12.24pm

--- src/Components/Survey/Survey.js
+++ src/Components/Survey/Survey.js
@@ -1,37 +1,31 @@
 import React from "react";
 import SurveyImg from "../../Assets/survey png.png";
-import SurveyBackgroundImage1 from "../../Assets/Rectangle 26.png";
-import SurveyBackgroundImage2 from "../../Assets/Vector.png";
-import { BsFillPlayCircleFill } from "react-icons/bs";
-import s from "./Survey.css";
+import SurveyBackgroundImage1 from "../../Assets/home_wave_vector.png";
+import s from "../Survey/survey.module.css";
 
 const Survey = () => {
   return (
-    <div className="survey-section-container">
-      <div className="survey-background-image-container-2">
-        <img src={SurveyBackgroundImage2} alt="" />
-      </div>
-      <div className="survey-background-image-container-1">
+    <div className={s.survey_section_container}>
+      <div className={s.survey_background_image_container_1}>
         <img src={SurveyBackgroundImage1} alt="" />
       </div>
-      <div className="survey-section-image-container">
-        <img src={SurveyImg} alt="" />
-      </div>
-      <div className="survey-section-text-container">
-        <p className="primary-subheading">Survey</p>
-        <h1 className="Secondary-heading">
-        Free Guidance to achieve your goals
-        </h1>
-        <p className="primary-text-1">
-        Ready to discover your career pathways? Longing to pursue a hobby professionally?
-         At Pathways.lk, we guide students and professionals to unlock their passions through a trailored guidance.
-          Ready to embark on a fulfilling journey?
-        </p>
-        <div className="survey-buttons-container">
-          <button className="secondary-button">Take Our Survey</button>
-          <button className="watch-video-button">
-            <BsFillPlayCircleFill /> Watch Video
-          </button>
+      <div className={s.survey_img_text_container}>
+        <div className={s.survey_section_image_container}>
+          <img src={SurveyImg} alt="" />
+        </div>
+        <div className={s.survey_section_text_container}>
+          <p className={s.primary_subheading}>Survey</p>
+          <h1 className={s.secondary_heading}>
+          Free Guidance to achieve your goals
+          </h1>
+          <p className={s.primary_text_1}>
+          Ready to discover your career pathways? Longing to pursue a hobby professionally?
+           At Pathways.lk, we guide students and professionals to unlock their passions through a trailored guidance.
+            Ready to embark on a fulfilling journey?
+          </p>
+          <div className={s.survey_buttons_container}>
+            <button className={s.secondary_button}>Take Our Survey</button>
+          </div>
         </div>
       </div>
     </div>
