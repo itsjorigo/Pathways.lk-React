b8e14b0f8f723cd209110cb8cd4cd48c1f307e47
Joseph Rodrigo <113199438+itsjorigo@users.noreply.github.com>
2023-09-04 17:37:55 +0530

    09.04.23 - 5.37pm

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,43 +1,48 @@
 import React from "react";
 import Joseph from "../../Assets/Joseph.png";
-import Nishadi from "../../Assets/Nishadi.png"
-import s from "./AboutUs.css";
+import Nishadi from "../../Assets/Nishadi.png";
+import s from "./about.module.css";
 
 const AboutUs = () => {
   return (
-    <div className="Roadmaps-section-wrapper">
-      <p className="primary-subheading">About Us</p>
-      <div className="Roadmaps-section-bottom">
-        <div className="Team-message-section">
-          <div className="Team-message">
-            <h1 className="primary-heading">A Message from Our Team at Pathways.lk</h1>
-            <p className="primary-text">
-            Embark on an enlightening academic journey with our
-            innovative website. As developers, we're thrilled to guide
-              you through undergrad to postgrad explorations.
-              Seamlessly navigate diverse pathways, gaining insights
-                into courses, industries, and careers. Our user-friendly
-                platform empowers confident, informed decisions,
-                  shaping your unique educational roadmap. Let's sculpt
-                  your destiny together – embrace boundless potential and
-                    step into a brighter future.
-            </p>
+    <div className={s.Team_message_section}>
+      <div className={s.Team_primary_subheading}>
+        <p>About Us</p>
+      </div>
+      <div className={s.Team_section_bottom}>
+        <div className={s.Team_message}>
+          <h1 className="primary-heading">
+            A Message from Our Team at Pathways.lk
+          </h1>
+          <p className="primary-text">
+            Embark on an enlightening academic journey with our innovative
+            website. As developers, we're thrilled to guide you through
+            undergrad to postgrad explorations. Seamlessly navigate diverse
+            pathways, gaining insights into courses, industries, and careers.
+            Our user-friendly platform empowers confident, informed decisions,
+            shaping your unique educational roadmap. Let's sculpt your destiny
+            together – embrace boundless potential and step into a brighter
+            future.
+          </p>
+        </div>
+        <div className={s.Team_person_card_container}>
+          <div className={s.person}>
+            <h2>Joseph Rodrigo</h2>
+            <div className={s.person_1_img}>
+              <img src={Joseph} alt="" />
             </div>
-        
-      
-      <div className="person-1">
-        <h2>Joseph Rodrigo</h2>
-          <div className="person-1-img"><img src={Joseph} alt="" /></div>
-          <div className="socialmedia-person-1"></div>
+            <div className={s.socialmedia_person_1}></div>
           </div>
-      <div className="person-2">
-        <h2>Nishadi Perera</h2>
-        <div className="person-2-img"><img src={Nishadi} alt="" /></div>
-        <div className="socialmedia-person-2"></div>
+          <div className={s.person}>
+             <h2>Nishadi Perera</h2>
+            <div className={s.person_2_img}>
+              <img src={Nishadi} alt="" />
+            </div>
+            <div className={s.socialmedia_person_2}></div>
+          </div>
+
         </div>
       </div>
-      <p>About Pathways.lk</p>
-    </div>
     </div>
   );
 };
