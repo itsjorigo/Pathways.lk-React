135774d786d4c924cd5a8b852bdfca1c89e9cb52
Nishadi Perera <124398259+nishadi220@users.noreply.github.com>
2023-09-05 15:53:37 +0530

    05.09.2023 - 03.53pm

---
+++ src/Components/Languages/Languages.js
@@ -0,0 +1,52 @@
+import React from "react";
+import s from "./Languages.module.css";
+
+const Languages = () => {
+  return (
+    <div className={s.grid_container}>
+      {/* 16 grid items */}
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+      <div className={s.grid_item}>
+        <div className={s.inner_grid}></div>
+        <div className={s.inner_grid}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Languages;
