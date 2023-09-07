07a16f4d4ede6154289ca2f53fe198d1fd189402
Nishadi Perera <124398259+nishadi220@users.noreply.github.com>
2023-09-07 20:15:13 +0530

    07.09.2023 - 8:15pm

--- src/Components/Qualifications/ALevel/alevel.js
+++ src/Components/Qualifications/ALevel/alevel.js
@@ -24,59 +24,13 @@ const Alevel = () => {
                 </div>
                 <div className={s.title}>
                   <a
-                    href="https://sliitacademy.lk/academic-programs/foundation-certificate-in-information-technology/"
-                    target="_blank"
-                  >
-                  Curtin university - SLIIT Academy Foundation Certificate in Information Technology</a>
+                    href="https://www.britishcouncil.lk/exam/school-exams"target="_blank">
+                  IGCSE, A-level</a>
                 </div>
-              </div>
-              <div className={s.inner_grid}>
-                <div className={s.pic}>
-                  <img src={SLTC} alt="" />
-                </div>
-                <div className={s.title}>
-                  <a href="https://sltc.ac.lk/courses/bsc-hons-in-software-engineering/" target="_blank">
-                    Bachelor of Software Engineering (Deakin University,
-                    Australia)
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className={s.grid_item}>
-              <div className={s.inner_grid}>
-              <div className={s.pic}>
-                  <img src={NSBM} alt="" />
-                </div>
-                <div className={s.title}>
-                  <a href="https://www.nsbm.ac.lk/course/bsc-hons-software-engineering/" target="_blank">
-                  BSc (Hons) IN Software Engineering
-                  Plymouth University – United Kingdom
-                  </a>
-                </div>
-              </div>
-              <div className={s.inner_grid}><div className={s.pic}>
-                <img src={IIT} alt="" />
-                </div>
-                <div className={s.title}>
-                  <a href="https://www.iit.ac.lk/course/beng-software-engineering/" target="_blank">
-                  BEng (Hons) Software Engineering - UGC Recognized
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className={s.grid_item}>
-              <div className={s.inner_grid}><div className={s.pic}>
-                  <img src={CINEC} alt="" />
-                </div>
-                <div className={s.title}>
-                  <a href="https://www.cinec.edu/courses/bsc-hons-in-software-engineering.html" target="_blank">
-                  BSc (Hons) in Software Engineering - Ministry of Education/ UGC Approved
-                  </a>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
+      </div>
+      </div>
       </div>
     );
   };
